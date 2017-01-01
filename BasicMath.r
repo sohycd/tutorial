2ba9affd961571bf529d5a0fb615ff6ddb8844a3
sohycd <sohycd@yahoo.com.sg>
2017-01-01 15:16:31 +0800

    Lesson 02

---
+++ BasicMath.r
@@ -0,0 +1,34 @@
+1 + 1
+1+2+3
+7-5
+8+9-6
+
+3*4
+4*3*2
+
+4/2
+4/3
+
+#PEMDAS
+
+4 * 6 + 5
+4 * (6 + 5)
+
+x <- 2
+x
+y = 5
+y
+z <- 3
+z
+a <- b <- 7
+a
+b
+
+assign("j",4)
+j
+
+rm(j)
+
+theVariable <- 17
+theVariable
+THEVARIABLE
